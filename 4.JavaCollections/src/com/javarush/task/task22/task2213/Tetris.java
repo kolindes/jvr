512a6e1b2dcd9c78ebe0e456cbbdd9f94b57fe90
HOME_PC <kolindes@icloud.com>
2019-12-14 18:01:33 +0300

    here's tetris begins

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -1,4 +1,31 @@
 package com.javarush.task.task22.task2213;
 
 public class Tetris {
+    static Tetris game;
+    private Field field;
+    private Figure figure;
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+
+    public void run() {}
+    public void step() {}
+
+    public static void main(String[] args) {
+        game = new Tetris();
+        game.run();
+    }
 }
