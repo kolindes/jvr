7877102a5524a1ce034e7af2b98a4306196ff835
HOME_PC <kolindes@icloud.com>
2020-01-28 12:45:18 +0300

    draw scene

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -3,5 +3,24 @@ package com.javarush.games.game2048;
 import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
+    private final static int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
 
+    private void drawScene() {
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                setCellColor(i, j, Color.YELLOW);
+            }
+        }
+    }
+
+    private void createGame() {
+    }
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+        createGame();
+        drawScene();
+    }
 }
